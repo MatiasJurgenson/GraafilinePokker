7b7386b0fc6067b2629d442e528a3ebb31658ddd
Joosep Suuder <115637478+jsgngie@users.noreply.github.com>
2023-05-09 19:35:49 +0300

    start ekraan
    Esimene ekraan mida kasutaja näeb + väike ümbertõste et asi töötaks.

--- src/main/java/com/example/graafilinepokker/Main.java
+++ src/main/java/com/example/graafilinepokker/Main.java
@@ -1,16 +1,20 @@
 package com.example.graafilinepokker;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -20,141 +24,77 @@ public class Main extends Application {
 
     @Override
     public void start(Stage pealava) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root);
 
-        //varastatud siit: https://coderslegacy.com/java/switch-between-scenes-in-javafx/
-        VBox layout = new VBox();
-        VBox layout2 = new VBox();
-        layout.setAlignment(Pos.CENTER);
-        layout2.setAlignment(Pos.CENTER);
+        pealava.setHeight(600);
+        pealava.setWidth(1000);
+        pealava.setTitle("easyPokker");
 
-        Scene scene = new Scene(layout, 300, 300);
-        Scene scene2 = new Scene(layout2, 300, 300);
+        //nupud
+        Rectangle backRuut = new Rectangle(300,75);
+        backRuut.setArcWidth(5.0);
+        backRuut.setArcHeight(5.0);
+        backRuut.setX(350);
+        backRuut.setY(175);
+        backRuut.setFill(Color.WHITESMOKE);
+        Label alusta = new Label("Alusta");
+        alusta.setFont(new Font("Comic Sans MS",40));
+        alusta.setLayoutY(180);
+        alusta.setLayoutX(440);
 
-        Label label1 = new Label("This is the First Scene");
-        Label label2 = new Label("This is the Second Scene");
+        Rectangle backRuut2 = new Rectangle(300,75);
+        backRuut2.setArcWidth(5.0);
+        backRuut2.setArcHeight(5.0);
+        backRuut2.setX(350);
+        backRuut2.setY(270);
+        backRuut2.setFill(Color.WHITESMOKE);
+        Label juhised = new Label("Juhised");
+        juhised.setFont(new Font("Comic Sans MS",40));
+        juhised.setLayoutX(420);
+        juhised.setLayoutY(275);
 
-        Button button = new Button("Forward");
-        button.setOnAction(e -> pealava.setScene(scene2));
+        Rectangle backRuut3 = new Rectangle(300,75);
+        backRuut3.setArcWidth(5.0);
+        backRuut3.setArcHeight(5.0);
+        backRuut3.setX(350);
+        backRuut3.setY(365);
+        backRuut3.setFill(Color.WHITESMOKE);
+        Label quit = new Label("Lahku");
+        quit.setFont(new Font("Comic Sans MS",40));
+        quit.setLayoutY(370);
+        quit.setLayoutX(440);
 
-        Button button2 = new Button("Backwards");
-        button2.setOnAction(e -> pealava.setScene(scene));
+        Label pealkiri = new Label("Pokker");
+        pealkiri.setTextFill(Color.FLORALWHITE);
+        pealkiri.setFont(new Font("Comic Sans MS",100));
+        pealkiri.setLayoutY(25);
+        pealkiri.setLayoutX(350);
 
-        TextField text = new TextField();
-        text.setMaxWidth(100);
-
-        layout.getChildren().addAll(label1, button);
-        layout2.getChildren().addAll(label2, button2, text);
-
-        pealava.setTitle("CodersLegacy");
+        root.getChildren().addAll(pealkiri,backRuut,backRuut2,backRuut3,alusta, quit, juhised);;
+        root.setBackground(new Background(
+                new BackgroundFill(Color.DARKGREEN, new CornerRadii(0), new Insets(0))));
         pealava.setScene(scene);
         pealava.show();
-    }
+        pealava.setResizable(false);
 
-    /* kui tulevikus edasi teha
+        scene.setOnMousePressed(event -> {
+            System.out.println("Mouse Pressed at (" + event.getX() + ", " + event.getY() + ")");
+            if (event.getX() >= 350 && event.getY() > 175 && event.getX() <= 650 && event.getY() <= 250) {
+                System.out.println("Mängi");
+                // siit edasi läheb mänguekraan ja loogika
 
-    Üks paar (One Pair) -ile kicker
-    Kaks paari (Two Pair) -ile kicker
-    ja teised täpsustavad asjad võiduolukordadele
+            } else if (event.getX() >= 350 && event.getY() > 270 && event.getX() <= 650 && event.getY() <= 345) {
+                System.out.println("Juhised");
+                // siit edasi lähevad juhised.
 
-    panustamine
-    */
-
-    /* reeglid
-     * mängu võitmine
-     * mängu võidab see kellel on kõige tugevam käsi käes
-     * käte tugevused on tegevaimast nõrgemani on järgmised
-     * https://et.wikipedia.org/wiki/Pokkerik%C3%A4te_tugevusj%C3%A4rjestus
-     * panustamine // lisab hiljem
-     */
-
-    /*
-    (done)    Programm käsitleb mingit (inimlikku) tegevust, näiteks mängimist
-    (done)    Programm peab kasutajalt midagi küsima (võib-olla ka korduvalt)
-    (done)    Programm peaks olema kasutatav ilma, et programmi kohta oleks erilisi eelteadmisi
-    (done)    Programm peab sisaldama juhusliku suuruse kasutamist (soovitavalt klassi Random abil)
-    (done)    Programm peab koosnema mitmest klassist (sh. peaklass)
-    (yes)     Programm peab olema rühmaliikmete endi kirjutatud
-        Programm peab olema mõistlikult kommenteeritud.
-    (kinda)    Programm ei tohiks olla liiga keeruline
-    (done)    Kasutajaga suhtlemine peaks olema väga elementaarselt kujundatud. Vältida (veel) graafilist kasutajaliidest.
-            Ekraanile kuvatav info (ka näiteks mänguseis) võib olla tekstiridadena (System.out.println).
-     */
-
-    public static void väljastaVõitja(List<Boolean> võitjad) {
-        System.out.println(võitjad);
-        int võitjateArv = 0;
-        List<Integer> võitjateIndex = new ArrayList<>();
-        for (int i = 0; i < võitjad.size(); i++) {
-            if (võitjad.get(i)) {
-                võitjateIndex.add(i);
-                võitjateArv++;
+            } else if (event.getX() >= 350 && event.getY() > 365 && event.getX() <= 650 && event.getY() <= 440) {
+                Platform.exit();
             }
-        }
+        });
 
-        if (võitjateArv > 1) {
-            System.out.println("Mäng jäi viiki mängijate poolt: ");
-            for (int i = 0; i < võitjateArv; i++) {
-                System.out.println((võitjateIndex.get(i)+1) + ". mängija.");
-            }
-            System.out.println("viigi põhjus: ");
-        } else {
-            System.out.println("Mängu võitis: " + (võitjateIndex.get(0)+1) + ". mängija.");
-            System.out.println("Võidu põhjus: ");
-        }
-    }
-    public static void kontrollija(Diiler diiler) {
-        if (diiler.royalStraightFlush().contains(true)) {
-            List<Boolean> võitjad = diiler.royalStraightFlush();
-            väljastaVõitja(võitjad);
-            System.out.println("Kuninglik mastirida");
 
-        } else if (diiler.straightFlush().contains(true)) {
-            List<Boolean> võitjad = diiler.straightFlush();
-            väljastaVõitja(võitjad);
-            System.out.println("Mastirida");
-
-        } else if (diiler.fourOfAKind().contains(true)) {
-            List<Boolean> võitjad = diiler.fourOfAKind();
-            väljastaVõitja(võitjad);
-            System.out.println("Nelik");
-
-        } else if (diiler.fullHouse().contains(true)) {
-            List<Boolean> võitjad = diiler.fullHouse();
-            väljastaVõitja(võitjad);
-            System.out.println("Maja");
-
-        } else if (diiler.flush().contains(true)) {
-            List<Boolean> võitjad = diiler.flush();
-            väljastaVõitja(võitjad);
-            System.out.println("Mast");
-
-        } else if (diiler.straight().contains(true)) {
-            List<Boolean> võitjad = diiler.straight();
-            väljastaVõitja(võitjad);
-            System.out.println("Rida");
-
-        } else if (diiler.threeOfAKind().contains(true)) {
-            List<Boolean> võitjad = diiler.threeOfAKind();
-            väljastaVõitja(võitjad);
-            System.out.println("Kolmik");
-
-        } else if (diiler.twoPair().contains(true)) {
-            List<Boolean> võitjad = diiler.twoPair();
-            väljastaVõitja(võitjad);
-            System.out.println("Kaks paar");
-
-        } else if (diiler.pair().contains(true)) {
-            List<Boolean> võitjad = diiler.pair();
-            väljastaVõitja(võitjad);
-            System.out.println("üks paar");
-        }
-    }
-
-    public static void main(String[] args) {
-        launch();
-
-        String tühik = "----------------\n";
-
+        /*
         //mängimise osa
         Scanner scan = new Scanner(System.in);
 
@@ -304,6 +244,7 @@ public class Main extends Application {
 
                 case 2:
                     //lisada mängu osa
+                    pealava.show();
                     Random random = new Random();
                     int mängijateArv = 0;
                     while (mängijateArv < 4) {
@@ -377,18 +318,122 @@ public class Main extends Application {
                         List<Boolean> võitjad = diiler.highCard();
                         väljastaVõitja(võitjad);
                         System.out.println("Kõrge kaart");
-                    } */
+                    }
 
-                System.out.println(tühik);
-                break;
-                    case 3:
+                    System.out.println(tühik);
+                    break;
+                case 3:
                     System.out.println("Aitäh, Mängige jälle!");
                     break mäng;
 
-                    }
+            }
+        }
+        */
+    }
+
+    /* kui tulevikus edasi teha
+
+    Üks paar (One Pair) -ile kicker
+    Kaks paari (Two Pair) -ile kicker
+    ja teised täpsustavad asjad võiduolukordadele
+
+    panustamine
+    */
+
+    /* reeglid
+     * mängu võitmine
+     * mängu võidab see kellel on kõige tugevam käsi käes
+     * käte tugevused on tegevaimast nõrgemani on järgmised
+     * https://et.wikipedia.org/wiki/Pokkerik%C3%A4te_tugevusj%C3%A4rjestus
+     * panustamine // lisab hiljem
+     */
+
+    /*
+    (done)    Programm käsitleb mingit (inimlikku) tegevust, näiteks mängimist
+    (done)    Programm peab kasutajalt midagi küsima (võib-olla ka korduvalt)
+    (done)    Programm peaks olema kasutatav ilma, et programmi kohta oleks erilisi eelteadmisi
+    (done)    Programm peab sisaldama juhusliku suuruse kasutamist (soovitavalt klassi Random abil)
+    (done)    Programm peab koosnema mitmest klassist (sh. peaklass)
+    (yes)     Programm peab olema rühmaliikmete endi kirjutatud
+        Programm peab olema mõistlikult kommenteeritud.
+    (kinda)    Programm ei tohiks olla liiga keeruline
+    (done)    Kasutajaga suhtlemine peaks olema väga elementaarselt kujundatud. Vältida (veel) graafilist kasutajaliidest.
+            Ekraanile kuvatav info (ka näiteks mänguseis) võib olla tekstiridadena (System.out.println).
+     */
+
+    public static void väljastaVõitja(List<Boolean> võitjad) {
+        System.out.println(võitjad);
+        int võitjateArv = 0;
+        List<Integer> võitjateIndex = new ArrayList<>();
+        for (int i = 0; i < võitjad.size(); i++) {
+            if (võitjad.get(i)) {
+                võitjateIndex.add(i);
+                võitjateArv++;
             }
         }
 
+        if (võitjateArv > 1) {
+            System.out.println("Mäng jäi viiki mängijate poolt: ");
+            for (int i = 0; i < võitjateArv; i++) {
+                System.out.println((võitjateIndex.get(i)+1) + ". mängija.");
+            }
+            System.out.println("viigi põhjus: ");
+        } else {
+            System.out.println("Mängu võitis: " + (võitjateIndex.get(0)+1) + ". mängija.");
+            System.out.println("Võidu põhjus: ");
+        }
+    }
+    public static void kontrollija(Diiler diiler) {
+        if (diiler.royalStraightFlush().contains(true)) {
+            List<Boolean> võitjad = diiler.royalStraightFlush();
+            väljastaVõitja(võitjad);
+            System.out.println("Kuninglik mastirida");
+
+        } else if (diiler.straightFlush().contains(true)) {
+            List<Boolean> võitjad = diiler.straightFlush();
+            väljastaVõitja(võitjad);
+            System.out.println("Mastirida");
+
+        } else if (diiler.fourOfAKind().contains(true)) {
+            List<Boolean> võitjad = diiler.fourOfAKind();
+            väljastaVõitja(võitjad);
+            System.out.println("Nelik");
+
+        } else if (diiler.fullHouse().contains(true)) {
+            List<Boolean> võitjad = diiler.fullHouse();
+            väljastaVõitja(võitjad);
+            System.out.println("Maja");
+
+        } else if (diiler.flush().contains(true)) {
+            List<Boolean> võitjad = diiler.flush();
+            väljastaVõitja(võitjad);
+            System.out.println("Mast");
+
+        } else if (diiler.straight().contains(true)) {
+            List<Boolean> võitjad = diiler.straight();
+            väljastaVõitja(võitjad);
+            System.out.println("Rida");
+
+        } else if (diiler.threeOfAKind().contains(true)) {
+            List<Boolean> võitjad = diiler.threeOfAKind();
+            väljastaVõitja(võitjad);
+            System.out.println("Kolmik");
+
+        } else if (diiler.twoPair().contains(true)) {
+            List<Boolean> võitjad = diiler.twoPair();
+            väljastaVõitja(võitjad);
+            System.out.println("Kaks paar");
+
+        } else if (diiler.pair().contains(true)) {
+            List<Boolean> võitjad = diiler.pair();
+            väljastaVõitja(võitjad);
+            System.out.println("üks paar");
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
 
 /* test tsükkel ära eemalda pls
